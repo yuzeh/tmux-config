9f8171a93ae90e89ac05de6a0df9129e45c44869
yuzeh <delta1080@gmail.com>
2017-09-14 11:33:23 -0700

    update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,8 +5,16 @@ die() {
   exit 1
 }
 
+set -e
+
 current_dir=`dirname $0`
+
+pushd $current_dir/vendor/basic-cpu-and-memory.tmux
+cmake .
+make
+make install
+popd
 
 [ `pwd` = "$HOME" ] || die "Please run this script from the home directory"
 
-ln -s "${current_dir}/.tmux.conf" ${HOME}/.tmux.conf"
+ln -s "${current_dir}/.tmux.conf" "${HOME}/.tmux.conf"
